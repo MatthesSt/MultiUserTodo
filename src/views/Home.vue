3d867fc1b65ba52b76d5d990338f2a2e609cd6db
Matthes Staacken <m.staacken@mbdteam.onmicrosoft.com>
2022-06-20 09:10:42 +0200

    stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <main>
+    <div class="d-flex justify-content-center align-items-center m-5">
+      <form @submit.prevent="addTodo()">
+        <input type="text" />
+        <button class="btn btn-success" type="submit">add Todo</button>
+      </form>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
@@ -10,8 +14,19 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   data() {
-    return {};
+    return {
+      todos: [] as todo[]
+    };
   },
-  methods: {},
+  methods: {
+    addTodo() {
+      console.log('todo');
+    },
+  },
 });
 </script>
+<style lang="scss" scoped>
+main {
+  height: 100vh;
+}
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,23 +4,31 @@
       <div class="card-header">Login</div>
       <div class="card-body">
         <form @submit.prevent="register()" autocomplete="off">
-          <div class="m-4 alert alert-danger text-center" v-if="error">Username or password is not correct</div>
+          <div class="m-4 alert alert-danger text-center" v-if="error">{{ error }}</div>
           <div class="form-group row mb-4">
             <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
             <div class="col-md-6">
-              <input id="email" type="email" name="email" value="" required autocomplete="email" class="form-control" />
+              <input id="email" type="email" name="email" v-model="email" required autocomplete="email" class="form-control" />
             </div>
           </div>
           <div class="form-group row mb-4">
             <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
             <div class="col-md-6">
-              <input id="password" type="password" name="password" required autocomplete="password" class="form-control" />
+              <input id="password" type="password" name="password" v-model="password" required autocomplete="password" class="form-control" />
             </div>
           </div>
           <div class="form-group row">
             <label for="confirmPassword" class="col-md-4 col-form-label text-md-right">Password bestätigen</label>
             <div class="col-md-6">
-              <input id="confirmPassword" type="password" name="confirmPassword" required autocomplete="password" class="form-control" />
+              <input
+                id="confirmPassword"
+                type="password"
+                name="confirmPassword"
+                v-model="confirmPassword"
+                required
+                autocomplete="password"
+                class="form-control"
+              />
             </div>
           </div>
           <button class="btn btn-success m-4 col-2" type="submit" v-if="!loggingIn">Registrieren</button>
@@ -39,21 +47,22 @@ export default defineComponent({
   data() {
     return {
       password: '',
+      confirmPassword: '',
       email: '',
-      error: false,
+      error: '',
       loggingIn: false,
     };
   },
   methods: {
     async register() {
-      this.error = false;
+      this.error = '';
 
-      if (!this.email) return (this.error = true);
-      if (!this.password) return (this.error = true);
+      if (!this.email) return (this.error = 'please enter an email');
+      if (!this.password) return (this.error = 'please enter a password');
+      if (this.password != this.confirmPassword) return (this.error = "passwords don't match");
       this.loggingIn = true;
       try {
         await API.register(this.email, this.password);
-        console.log('logged in with:' + this.email);
         this.email = '';
         this.password = '';
         this.$router.push('/home');
@@ -61,7 +70,7 @@ export default defineComponent({
         console.error({ "couldn't login": e });
       } finally {
         this.password = '';
-        this.error = true;
+        this.error = '';
         this.loggingIn = false;
       }
     },
