dc9df1b623da3986a9a5a781f8d6b72a1f2344a7
Matthes Staacken <m.staacken@mbdteam.onmicrosoft.com>
2022-06-20 15:55:48 +0200

    stuff

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,18 @@
+type id = string;
 export interface todo {
-  id: number;
+  id: id;
   name: string;
   priority: number;
   done: boolean;
+  //   expGain
 }
-
+export interface todoList {
+  name: string;
+  id: id;
+  creatorId: id;
+  todos: {
+    [todoId: string]: todo;
+  };
+  //   authorizedUsers: id[];
+}
 export const typescriptFix = 1;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,52 @@
 <template>
   <main>
-    <div class="d-flex justify-content-center align-items-center m-5">
+    <div>
+      <form @submit.prevent="createNewList()">
+        <input v-model="newListName" type="text" placeholder="name" />
+        <button class="btn btn-success">create new list</button>
+      </form>
+    </div>
+    <div v-if="lists.length" class="d-flex justify-content-center align-items-center m-5">
       <form @submit.prevent="addTodo()">
         <input type="text" placeholder="name" v-model="newToDoName" />
         <input type="number" placeholder="priority" v-model="priority" />
+        <select name="list" id="list" v-model="selectedList">
+          <option v-for="list in lists" :key="list.id" :value="list.id">{{ list.name }}</option>
+        </select>
         <button class="btn btn-success" type="submit">add Todo</button>
       </form>
     </div>
-    <div class="maingrid">
-      <div class="d-flex flex-column justify-content-start">
-        <h3>ToDos</h3>
-        <div v-for="todo in todos.filter(t => !t.done).sort(todoSortPriority)" :key="todo.name">
-          {{ todo }}
-          <input type="checkbox" class="btn btn-success" @change="todo.done = true" />
+    <div v-for="list in lists" :key="list.id">
+      <h2>{{ list.name }}</h2>
+      <div class="maingrid">
+        <div class="d-flex flex-column justify-content-start">
+          <h3>ToDos</h3>
+          <div>
+            <div
+              v-for="todo in Object.values(list.todos || {})
+                .filter(t => !t.done)
+                .sort(todoSortPriority)"
+              :key="todo.name"
+            >
+              {{ todo }}
+              <input type="checkbox" class="btn btn-success" @change="todo.done = true" />
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="d-flex flex-column justify-content-start">
-        <h3>Done</h3>
-        <div v-for="todo in todos.filter(t => t.done).sort(todoSortPriority)" :key="todo.name">
-          {{ todo }}
-          <button class="btn btn-danger" @click="deleteTodo(todo.id)">X</button>
-          <button class="btn btn-success" @click="todo.done = false">&#x2713;</button>
+        <div class="d-flex flex-column justify-content-start">
+          <h3>Done</h3>
+          <div>
+            <div
+              v-for="todo in Object.values(list.todos || {})
+                .filter(t => t.done)
+                .sort(todoSortPriority)"
+              :key="todo.name"
+            >
+              {{ todo }}
+              <button class="btn btn-danger" @click="deleteTodo(todo.id)">X</button>
+              <button class="btn btn-success" @click="todo.done = false">&#x2713;</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -29,32 +55,57 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { todo, typescriptFix } from '@/types';
+import { todo, todoList, typescriptFix } from '@/types';
+import * as API from '@/API';
+import { currentUser } from '@/router';
 
 export default defineComponent({
   data() {
     return {
+      error: '',
+      newListName: '',
       todos: [] as todo[],
+      lists: [] as todoList[],
       doneTodos: [] as todo[],
       newToDoName: '',
+      selectedListId: '',
       priority: null as number | null,
     };
   },
-  mounted() {
+  computed: {
+    selectedList(): todoList | undefined {
+      return this.lists.find(l => l.id == this.selectedListId);
+    },
+  },
+  async mounted() {
     console.log(typescriptFix);
+    this.lists = await API.getTodoLists();
   },
   methods: {
+    createNewList() {
+      this.lists.push({ name: this.newListName, id: Math.random() + '', creatorId: currentUser.value!.uid, todos: {} });
+    },
     addTodo() {
+      if (!this.selectedList) return;
       if (this.newToDoName && this.priority != null && typeof this.priority == 'number') {
-        this.todos.push({
-          name: this.newToDoName,
-          priority: this.priority,
-          done: false,
-          id: Math.random(),
-        });
+        let todo = { name: this.newToDoName, priority: this.priority, done: false, id: Math.random() + '' };
+        this.selectedList.todos[todo.id] = todo;
+        try {
+          API.addTodo(this.selectedList);
+        } catch (e) {
+          this.error = "couldn't upload todo";
+        }
+        this.todos.push(todo);
       }
     },
-    deleteTodo(id: number) {
+    // async updateTodo(todo: todo) {
+    //   try {
+    //     await API.updateTodo(todo);
+    //   } catch (e) {
+    //     this.error = "couldn't update";
+    //   }
+    // },
+    deleteTodo(id: string) {
       this.todos = this.todos.filter(t => t.id != id);
     },
     todoSortPriority(a: todo, b: todo) {
