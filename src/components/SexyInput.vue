2caa6a9c2fa13314f29a1079b97c57847f4fbd4f
tim <t.littschwager@mbd-team.de>
2022-06-23 13:49:24 +0200

    new sexy inputs

--- src/components/SexyInput.vue
+++ src/components/SexyInput.vue
@@ -22,13 +22,9 @@
       :id="id"
       :list="id2"
       autocomplete="off"
+      :required="type == 'date' || type == 'time'"
     />
     <!-- /standard input field -->
-    <!-- multiSelect list -->
-    <div v-for="multi of multiSelect" :key="JSON.stringify(multi)" class="mb-1">
-      {{ multi }}
-    </div>
-    <!-- /multiSelect list -->
     <!-- options for datalist -->
     <div :id="wrapperId" class="simple-typeahead input-contain" v-if="type == 'select' || type == 'multiSelect'">
       <input
@@ -75,7 +71,7 @@
           v-for="(item, index) in filteredItems"
           :key="index"
           @mousedown.prevent
-          @click="selectItem(item)"
+          @click.stop="selectItem(item)"
           @mouseenter="currentSelectionIndex = index"
         >
           <span class="simple-typeahead-list-item-text" :data-text="optionProjection(item)" v-if="$slots['list-item-text']">
@@ -95,6 +91,22 @@
       </div>
     </div>
     <!-- /options for datalist -->
+    <!-- multiSelect list -->
+    <div v-for="(multi, index) of multiSelect" :key="JSON.stringify(multi)" class="mb-1 d-flex justify-content-between px-2">
+      {{ multi }}
+      <span @click="$emit('deleteItem', index)">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+          <path
+            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
+          />
+          <path
+            fill-rule="evenodd"
+            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+          />
+        </svg>
+      </span>
+    </div>
+    <!-- /multiSelect list -->
     <!-- standard textarea field -->
     <textarea
       v-if="type == 'textarea'"
@@ -107,7 +119,7 @@
     ></textarea>
     <!-- /standard textarea field -->
     <!-- label for select -->
-    <label class="placeholder-text" v-if="type != 'select'">
+    <label class="placeholder-text" v-if="type != 'select' && type != 'multiSelect'">
       <div class="text" :style="labelStyle" :class="{ withBorder: labelBorder }">
         {{ placeholder }}
       </div>
@@ -137,7 +149,7 @@
     />
     <!-- /sideInput for rangeInput -->
     <!-- sideButton -->
-    <button v-if="btnText" type="button" @click="affirm()" :class="btnClass" :style="btnStyle">
+    <button v-if="btnText" type="button" @click="affirm()" :class="btnClass">
       {{ btnText }}
     </button>
     <!-- /sideButton -->
@@ -179,7 +191,9 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   setup() {
-    return;
+    return {
+      console,
+    };
   },
   props: {
     placeholder: {
@@ -200,8 +214,8 @@ export default defineComponent({
     btnClass: {
       type: String,
     },
-    btnStyle: {
-      type: String,
+    btnAction: {
+      type: Function,
     },
     sideInputType: {
       type: String,
@@ -223,7 +237,7 @@ export default defineComponent({
     },
     sideWidth: {
       type: String,
-      default: '20',
+      default: '20%',
     },
     labelStyle: {
       type: String,
@@ -231,9 +245,6 @@ export default defineComponent({
     labelBorder: {
       type: Boolean,
       default: false,
-    },
-    btnAction: {
-      type: Function,
     },
     options: {
       type: Array,
@@ -259,7 +270,7 @@ export default defineComponent({
     },
     noElementMessage: {
       type: String,
-      default: '',
+      default: 'not found',
     },
     controlInput: {
       type: Boolean,
@@ -303,9 +314,8 @@ export default defineComponent({
       //options that are still possible
       const regexp = new RegExp(this.escapeRegExp(this.modelValue), 'i');
       let array = this.options!.filter(item => this.optionProjection(item).match(regexp));
-
       if (!array.length) array = array.concat(this.options!.filter((item: any) => item.match(regexp)));
-      return array;
+      return array.filter(e => !this.multiSelect?.includes(e));
     },
     currentSelection() {
       //the option which is currently selected
@@ -382,12 +392,15 @@ export default defineComponent({
       if (typeof event == 'string') {
         this.$emit('update:modelValue', event);
       } else {
-        this.$emit('update:modelValue', event.target.value);
+        if (this.type == 'number') {
+          this.$emit('update:modelValue', event.target.value * 1);
+        } else {
+          this.$emit('update:modelValue', event.target.value);
+        }
       }
     },
     updateSideValue(event: any) {
       //update the sideInput value
-
       this.$emit('update:sideInputVModel', event.target.value);
     },
     async affirm() {
@@ -422,9 +435,12 @@ export default defineComponent({
     onBlur() {
       //is executed when the selectInput is no longer focused
       this.isListVisible = false;
-      if (this.type != 'select') return;
+      if (this.type != 'select' && this.type != 'multiSelect') return;
       if (this.controlInput) {
-        if (!this.options?.some(e => this.optionProjection(e) == this.modelValue)) this.updateValue('');
+        if (!this.filteredItems?.some(e => this.optionProjection(e) == this.modelValue)) {
+          this.updateValue('');
+          return;
+        }
       }
       if (this.selectOnBlur) {
         if (this.options?.find(e => this.optionProjection(e) == this.modelValue)) {
@@ -477,7 +493,7 @@ export default defineComponent({
       await this.updateValue(this.optionProjection(item));
       this.currentSelectionIndex = 0;
       document.getElementById(this.id)!.blur();
-      this.$emit('selectItem', item);
+      if (!this.selectOnBlur) this.$emit('selectItem', item);
     },
     escapeRegExp(string: string) {
       //filters unwanted characters from a string
